c3a39e60b2e51b8f80e30d6459123853b58ce4e0
Christian Tzolov <christian.tzolov@broadcom.com>
2025-08-15 10:07:20 +0200

    Improve reference docs
    Signed-off-by: Christian Tzolov <christian.tzolov@broadcom.com>

---
+++ auto-configurations/mcp/spring-ai-autoconfigure-mcp-streamable-server-webflux/src/test/java/org/springframework/ai/mcp/server/streamable/webflux/autoconfigure/BlaIT.java
@@ -0,0 +1,14 @@
+package org.springframework.ai.mcp.server.streamable.webflux.autoconfigure;
+
+import org.springframework.ai.mcp.client.webflux.autoconfigure.SseWebFluxTransportAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+public class BlaIT {
+
+	// private final ApplicationContextRunner contextRunner = new
+	// ApplicationContextRunner()
+	// .withConfiguration(AutoConfigurations.of(McpStreamableServerWebFluxAutoConfiguration.class,
+	// SseWebFluxTransportAutoConfiguration.class));
+
+}
